0532d1153378e6ad14629afd26077478f090704b
Impluse2 <golusrkaiiii06@gmail.com>
2024-10-20 13:16:22 +0300

    Update loader.py

--- tg_bot/loader.py
+++ tg_bot/loader.py
@@ -1,11 +1,9 @@
 from aiogram import Bot, Dispatcher
-import logging
+
 
 TG_TOKEN = ('8046045883:AAG17Ohsln323bbOk0B_MRxuYlqnVRU-InM')
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 
-def on_startup():
-    print('Бот запущен')
 
 
